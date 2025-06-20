affc960d454a018919b2a8d62da8719384503416
Sebi <sebilune@duck.com>
2025-06-20 15:39:47 -0400

    feat: add shadcn/ui with popover demo component

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import { createRoot } from "react-dom/client";
-import { Simulation } from "./components/Simulation";
+import { Simulation } from "@/components/Simulation";
+import { Settings } from "@/components/Settings";
 import "./index.css";
 
 function App() {
   return (
-    <div className="w-screen h-screen">
+    <div className="w-screen h-screen relative">
       <Simulation />
+      <div className="absolute top-4 right-4 z-10">
+        <Settings />
+      </div>
     </div>
   );
 }
