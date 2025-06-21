09489d9bc0f92c87d88801441dc729ffb0db43dc
Sebi <sebilune@duck.com>
2025-06-21 17:21:40 -0400

    tune: default particle scale and world proportion

--- src/main.tsx
+++ src/main.tsx
@@ -30,9 +30,9 @@ function App() {
     timeScale: 10.0,
     wallRepel: 0.125,
     wallForce: 0.01,
-    particleSize: 12.0,
+    particleSize: 6.0,
     useProportionalScaling: true,
-    refPopulation: 1200,
+    refPopulation: 1000,
     scalingRatio: 0.5,
     mouseRepel: true,
   };
