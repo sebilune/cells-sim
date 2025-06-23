6b898c32b2ed03ca4d67ed81bfd8e080b83fd640
Sebi <sebilune@duck.com>
2025-06-23 19:29:47 -0400

    feat: it moves!

--- src/config/worlds.ts
+++ src/config/worlds.ts
@@ -1,5 +1,3 @@
 export const worlds: string[] = [
-  "2xAaCEnCSgxMyTywvGZLd26mQRLDyONpjkSSHOibkscPIVd",
-  "1ciPbDgLcXFSF5VwvfTzglE3jQ7vaTlscDlBeCRVLjf5StQ",
-  "1IC4rxuDs7e72GFyC1LayJD32rhx45HcovuRvBziCJEC3JU",
+  "0lYmOPseyWWa4YjNOpiArTZskULgvsz1AyZ8694NovTTJyv",
 ];
