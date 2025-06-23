2263520688e6711f0b74b911aa26c741488e287e
Sebi <sebilune@duck.com>
2025-06-23 19:10:31 -0400

    feat: add new worlds seed

--- src/config/worlds.ts
+++ src/config/worlds.ts
@@ -2,4 +2,5 @@ export const worlds: string[] = [
   "255MAXRZAd40U08Ga6R03hABBM7LPEMSAwtPpShEt1oMW3u",
   "2GwJVl8ev5wJZNWrvcWbdotMuOE66oKPJPm6R4VBzlHSvVQ",
   "2xAaCEnCSgxMyTywvGZLd26mQRLDyONpjkSSHOibkscPIVd",
+  "1ciPbDgLcXFSF5VwvfTzglE3jQ7vaTlscDlBeCRVLjf5StQ",
 ];
