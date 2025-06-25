9ad3831be6de81caab3112df3791d2c9c19845b0
Sebi <sebilune@duck.com>
2025-06-24 22:24:30 -0400

    chore: remove unused mode param

--- vite.config.ts
+++ vite.config.ts
@@ -3,47 +3,45 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [
-      react({
-        babel: {
-          plugins: [["babel-plugin-react-compiler"]],
-        },
-      }),
-      tailwindcss(),
-    ],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler"]],
+      },
+    }),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    sourcemap: false,
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+        passes: 2,
+      },
+      mangle: {
+        toplevel: true,
+      },
+      keep_classnames: false,
+      keep_fnames: false,
+      format: {
+        comments: false,
       },
     },
-    build: {
-      sourcemap: false,
-      minify: "terser",
-      terserOptions: {
-        compress: {
-          drop_console: true,
-          drop_debugger: true,
-          passes: 2,
-        },
-        mangle: {
-          toplevel: true,
-        },
-        keep_classnames: false,
-        keep_fnames: false,
-        format: {
-          comments: false,
-        },
-      },
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            react: ["react", "react-dom"],
-            regl: ["regl"],
-          },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          regl: ["regl"],
         },
       },
     },
-  };
+  },
 });
